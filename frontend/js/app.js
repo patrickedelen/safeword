0b5622ef949f539e4f69705e5a058a0254e86c3a
killvung <kiulam125@gmail.com>
2016-04-16 19:54:20 -0500

    Getting no where

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,20 +1,6 @@
 $(document).ready(function(){
         $(".button-collapse").sideNav();
-        // make some waves.
-        var ocean = document.getElementById("ocean"),
-            waveWidth = 10,
-            waveCount = Math.floor(window.innerWidth/waveWidth),
-            docFrag = document.createDocumentFragment();
-
-        for(var i = 0; i < waveCount; i++){
-          var wave = document.createElement("div");
-          wave.className += " wave";
-          docFrag.appendChild(wave);
-          wave.style.left = i * waveWidth + "px";
-          wave.style.webkitAnimationDelay = (i/100) + "s";
-        }
-
-        ocean.appendChild(docFrag);
+        
 });
 
 (function(){
